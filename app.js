0b4fa6dc95fa99495798fd07bcecd1013e082498
Wasino97 <salazarjaycee@gmail.com>
2022-01-27 15:34:57 -0600

    save 1 | picking random quotes

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const keyBoard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByTagName('a')[0];
 
-let missed = 0
+let missed = 0;
+
+//Start Game Button 
+btnReset.addEventListener('click', (e) => {
+    const overlay = document.getElementsByClassName('start')[0];
+    overlay.style.display = "none";
+});
+
+const phraseArray = [
+    "Can't make a tomlett without breaking a couple greggs",
+    "You're my boy you're my number one boy",
+    'Then how about tom with some fucking greg sprinkles',
+    'Connor roy was intrested in politics from a very young age',
+    'We just walked in on mom and dad fucking us'
+];
+
+
+function getRandomPhraseAsArray(arr){
+   let phrase = phraseArray[Math.floor(Math.random() * phraseArray.length)];
+   console.log(phrase);
+};
+
